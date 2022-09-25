92f6000b9c91715f267b52d8846c93ec3600f498
jatinjohar11 <80395051+jatinjohar11@users.noreply.github.com>
2022-09-25 19:51:46 +0530

    Add files via upload

---
+++ Assingment_2/A2Q1.java
@@ -0,0 +1,8 @@
+public class A2Q1{
+    public static void main(String args[]){
+        double score = 81.79d;
+        if(score>=80 && score<=90){
+            System.out.println(score+5);
+        }
+    }
+}
---
+++ Assingment_2/A2Q6.java
@@ -0,0 +1,10 @@
+public class A2Q6 {
+    public static void main(String args[]){
+        int a=3;
+        do{
+            System.out.print(a+" ");
+            a+=3;
+        }
+        while(a<=36);
+    }
+}
